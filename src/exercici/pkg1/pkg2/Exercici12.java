38f4cc86d49d9cc010c983a5f8e43ba40fb5f227
davidblancolopez <daviddbl94@gmail.com>
2017-01-26 15:53:58 +0100

    CAMBIOS

--- src/exercici/pkg1/pkg2/Exercici12.java
+++ src/exercici/pkg1/pkg2/Exercici12.java
@@ -1,12 +1,26 @@
 
 package exercici.pkg1.pkg2;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 
 public class Exercici12 {
 
 
     public static void main(String[] args) {
-        
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+           
+                NetworkInterface interfaz = interfaces.nextElement();
+                System.out.println("Interfaz: " + interfaz.getName());
+                byte[] direccion = interfaz.getHardwareAddress();
+
+        } catch(SocketException e) {
+            System.out.println(e);
+        }
     }
     
 }
